e69740fe0f211eed376e7543e7898a2f73ae626c
whdms2008 <45191177+whdms2008@users.noreply.github.com>
2024-07-08 14:46:58 +0900

    refactor: 사용자 입력 유도 최적화

--- pyramid.py
+++ pyramid.py
@@ -3,6 +3,7 @@ def print_pyramid(height):
         print(' ' * (height - i - 1), end='')
 
         print('*' * (2 * i + 1))
-
-height = int(input("이게 뭘까? : "))
+        
+# 출력할 피라미드의 높이를 입력하는 값
+height = int(input("출력할 피라미드이 높이 : "))
 print_pyramid(height)
