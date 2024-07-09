0ea36c7f9867c8efd50be3002308033e9538ef38
whdms2008 <45191177+whdms2008@users.noreply.github.com>
2024-07-09 12:16:05 +0900

    chore: pyramid.py 위치를 Andrew 폴더 하위로 이동

--- pyramid.py
+++
@@ -1,9 +0,0 @@
-def print_pyramid(height):
-    for i in range(height):
-        print(' ' * (height - i - 1), end='')
-
-        print('*' * (2 * i + 1))
-        
-# 출력할 피라미드의 높이를 입력하는 값
-height = int(input("출력할 피라미드이 높이 : "))
-print_pyramid(height)
